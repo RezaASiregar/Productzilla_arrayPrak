87078de5a0eb1741cbc32315fe102c300f9d5cdf
RezaASiregar <rezasiregar98@gmail.com>
2023-03-07 21:22:51 +0700

    tugas 2

---
+++ array-2.js
@@ -0,0 +1,19 @@
+// soal 2
+let liverpool = [
+    ['Nunez', 'Gakpo', 'Salah', 'Firmino', 'Diaz'],
+    ['Gerrard', 'Thiago', 'Henderson', 'Elliot', 'Fabinho'],
+    ['VDJ', 'Robertson', 'TAA', 'Konate', 'Tsimikas'],
+]
+
+console.log('========soal 2========')
+console.log(liverpool[0][0]);
+console.log(liverpool[0][2]);
+console.log(liverpool[0][4]);
+
+console.log(liverpool[1][0]);
+console.log(liverpool[1][2]);
+console.log(liverpool[1][4]);
+
+console.log(liverpool[2][0]);
+console.log(liverpool[2][2]);
+console.log(liverpool[2][4]);
