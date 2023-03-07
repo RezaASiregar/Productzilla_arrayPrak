535d33042c0e9af9d8e301d24f1fa0924fdd7bf4
RezaASiregar <rezasiregar98@gmail.com>
2023-03-07 20:37:02 +0700

    array prak

---
+++ array.js
@@ -0,0 +1,10 @@
+let penyerang = ['Nunez', 'Gakpo', 'Salah', 'Firmino', 'Diaz']
+let gelandang = ['Gerrard', 'Thiago', 'Henderson', 'Elliot', 'Fabinho']
+let back = ['VDJ', 'Robertson', 'TAA', 'Konate', 'Milner']
+
+console.log(penyerang[1]);
+console.log(penyerang[3]);
+console.log(gelandang[1]);
+console.log(gelandang[3]);
+console.log(back[1]);
+console.log(back[3]);
